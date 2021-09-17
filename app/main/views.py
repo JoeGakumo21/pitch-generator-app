bfe82d66df0f335027f18a998cd5e49e30fba126
Joe Gakumo <joegakumo1@gmail.com>
2021-09-17 16:28:26 +0300

    add user profile

--- app/main/views.py
+++ app/main/views.py
@@ -37,5 +37,15 @@ def pitches():
 
     return render_template('pitches.html',title=title)
 
+@main.route('/user/<uname>')
+@login_required
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)    
+
 
 
