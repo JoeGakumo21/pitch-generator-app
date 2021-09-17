5bb8310bbe9e5e60e500fe7973820f25bc59eff2
Joe Gakumo <joegakumo1@gmail.com>
2021-09-17 15:51:19 +0300

    imported function get_pitches

--- app/main/views.py
+++ app/main/views.py
@@ -5,13 +5,14 @@ from ..models import User
 from flask_login import login_required,current_user
 from .. import db,photos
 import markdown2  
+from ..request import get_pitches;
 
 
 
 # Views
 
 @main.route('/')
-@login_required
+# @login_required
 def index():
 
     '''
@@ -24,6 +25,17 @@ def index():
         
     return render_template('index.html')
 
+@main.route('/pitches')
+def pitches():
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    
+    # reviews = Review.get_reviews(movie.id)
+    pitches=get_pitches()
+
+    return render_template('pitches.html',pitches=pitches)
 
 
 
