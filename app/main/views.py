9d8230739bf5efcc159a71677982871d057870a3
Joe Gakumo <joegakumo1@gmail.com>
2021-09-17 16:30:22 +0300

    add updated picture

--- app/main/views.py
+++ app/main/views.py
@@ -67,3 +67,22 @@ def update_profile(uname):
 
 
 
+@main.route('/user/<uname>/update/pic',methods = ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname = uname))
+
+    
+
+
+
+
+
+
+
