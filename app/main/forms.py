521605fb08f460dd13ee43849fac907045d6bf60
Joe Gakumo <joegakumo1@gmail.com>
2021-09-18 00:54:12 +0300

    create class pitchform and comments

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField
+from wtforms import StringField,TextAreaField,SubmitField,SelectField
 from wtforms.validators import Required
 
 class ReviewForm(FlaskForm):
@@ -11,3 +11,15 @@ class ReviewForm(FlaskForm):
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
     submit = SubmitField('Submit')
+
+
+class PitchForm(FlaskForm):
+  title = StringField('Pitch title')
+  category = SelectField("Choose Category",choices=[('Brand','Personal brand'),('Product','Product'),('Project','Project'),('Investor','Investor')])
+  pitch = TextAreaField('Your Pitch',validators=[Required()])
+  submit = SubmitField('Submit')
+
+
+class CommentForm(FlaskForm):
+  comment = TextAreaField('Your Comment',validators=[Required()])
+  submit = SubmitField('Submit') 
