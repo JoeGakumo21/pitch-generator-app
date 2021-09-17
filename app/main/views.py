0ff699a7338dc54d615b6c22ddfd7580e1aa2856
Joe Gakumo <joegakumo1@gmail.com>
2021-09-17 13:22:19 +0300

    minor charges

---
+++ app/main/views.py
@@ -0,0 +1,28 @@
+from flask import render_template,request,redirect,url_for,abort
+from . import main
+from .forms import ReviewForm, UpdateProfile
+from ..models import User
+from flask_login import login_required,current_user
+from .. import db,photos
+import markdown2  
+
+
+
+# Views
+
+@main.route('/')
+@login_required
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+
+    '''
+
+    # Getting popular movie
+   
+        
+    return render_template('index.html')
+
+
+
---
+++ config.py
@@ -0,0 +1,67 @@
+import os
+class Config:
+
+    '''
+    General configuration parent class
+    
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/mypitches'
+    SECRET_KEY = os.urandom(32)
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+   
+    
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+
+
+class DevConfig(Config):
+    
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+
+
+config_options = {
+
+    'development':DevConfig,
+    'production':ProdConfig,
+    
+}    
+
+
+
+
+
+
