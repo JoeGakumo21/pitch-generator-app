4dc0cb29c535383544d7668034cd7baead836229
Joe Gakumo <joegakumo1@gmail.com>
2021-09-19 01:40:58 +0300

    second deployment to heroku

--- config.py
+++ config.py
@@ -20,8 +20,8 @@ class Config:
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
     # simple mde  configurations
-    SIMPLEMDE_JS_IIFE = True
-    SIMPLEMDE_USE_CDN = True
+    # SIMPLEMDE_JS_IIFE = True
+    # SIMPLEMDE_USE_CDN = True
 
 
 
